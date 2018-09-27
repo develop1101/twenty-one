ae94451a970542ad919aa962bdf8fbfbbaa725b7
develop1101 <43621205+develop1101@users.noreply.github.com>
2018-09-27 11:41:51 -0400

    Add files

---
+++ TwentyOneGame/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TwentyOneGame
+{
+    partial class TwentyOne
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TwentyOne));
+            this.lblDice1 = new System.Windows.Forms.Label();
+            this.lblDice2 = new System.Windows.Forms.Label();
+            this.lblDice3 = new System.Windows.Forms.Label();
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTotal1 = new System.Windows.Forms.Label();
+            this.lblTotal2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotal3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDice1
+            // 
+            this.lblDice1.BackColor = System.Drawing.Color.DarkGray;
+            this.lblDice1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDice1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDice1.Location = new System.Drawing.Point(250, 211);
+            this.lblDice1.Name = "lblDice1";
+            this.lblDice1.Size = new System.Drawing.Size(131, 124);
+            this.lblDice1.TabIndex = 0;
+            this.lblDice1.Text = "Dice 1";
+            this.lblDice1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice2
+            // 
+            this.lblDice2.BackColor = System.Drawing.Color.DarkGray;
+            this.lblDice2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDice2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDice2.Location = new System.Drawing.Point(387, 211);
+            this.lblDice2.Name = "lblDice2";
+            this.lblDice2.Size = new System.Drawing.Size(120, 124);
+            this.lblDice2.TabIndex = 1;
+            this.lblDice2.Text = "Dice 2";
+            this.lblDice2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice3
+            // 
+            this.lblDice3.BackColor = System.Drawing.Color.DarkGray;
+            this.lblDice3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDice3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDice3.Location = new System.Drawing.Point(513, 211);
+            this.lblDice3.Name = "lblDice3";
+            this.lblDice3.Size = new System.Drawing.Size(129, 124);
+            this.lblDice3.TabIndex = 2;
+            this.lblDice3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRollDice.ForeColor = System.Drawing.Color.Red;
+            this.btnRollDice.Location = new System.Drawing.Point(648, 211);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(144, 57);
+            this.btnRollDice.TabIndex = 3;
+            this.btnRollDice.Text = "Roll the Dice";
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Blue;
+            this.btnReset.Location = new System.Drawing.Point(683, 278);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(109, 57);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTotal1
+            // 
+            this.lblTotal1.BackColor = System.Drawing.Color.Yellow;
+            this.lblTotal1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal1.Location = new System.Drawing.Point(623, 353);
+            this.lblTotal1.Name = "lblTotal1";
+            this.lblTotal1.Size = new System.Drawing.Size(169, 38);
+            this.lblTotal1.TabIndex = 5;
+            this.lblTotal1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotal2
+            // 
+            this.lblTotal2.BackColor = System.Drawing.Color.Yellow;
+            this.lblTotal2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal2.Location = new System.Drawing.Point(623, 405);
+            this.lblTotal2.Name = "lblTotal2";
+            this.lblTotal2.Size = new System.Drawing.Size(169, 39);
+            this.lblTotal2.TabIndex = 6;
+            this.lblTotal2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(401, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total for the First Roll: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(401, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Total for the Second Roll: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(401, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Total for the Third Roll: ";
+            // 
+            // lblTotal3
+            // 
+            this.lblTotal3.BackColor = System.Drawing.Color.Yellow;
+            this.lblTotal3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal3.Location = new System.Drawing.Point(623, 457);
+            this.lblTotal3.Name = "lblTotal3";
+            this.lblTotal3.Size = new System.Drawing.Size(169, 39);
+            this.lblTotal3.TabIndex = 9;
+            this.lblTotal3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(242, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(550, 112);
+            this.label4.TabIndex = 11;
+            this.label4.Text = resources.GetString("label4.Text");
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TwentyOne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 508);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTotal3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal2);
+            this.Controls.Add(this.lblTotal1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRollDice);
+            this.Controls.Add(this.lblDice3);
+            this.Controls.Add(this.lblDice2);
+            this.Controls.Add(this.lblDice1);
+            this.Name = "TwentyOne";
+            this.Text = "TwentyOne";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDice1;
+        private System.Windows.Forms.Label lblDice2;
+        private System.Windows.Forms.Label lblDice3;
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTotal1;
+        private System.Windows.Forms.Label lblTotal2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotal3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TwentyOneGame/Form1.cs
@@ -0,0 +1,235 @@
+﻿//==========================================================================
+// Twenty One Game
+// This program lets the user roll the two dices twice! 
+// If the total number of the first roll and the second roll is 21, you win.
+// If the total number is equal to or more than 15, you can roll the Third dice.
+// When you roll the third dice, you win if the total number of the first roll and
+// the second roll and the the number of the third dice equal to 21.
+//=============================================================================//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwentyOneGame
+{
+    public partial class TwentyOne : Form
+    {
+        // variables for Dice1, Dice2, and Dice3
+        int dice1, dice2, dice3;
+
+        // variable for a total number
+        int total = 0;
+
+        // create a Random object
+        Random random = new Random();
+
+        public TwentyOne()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //********************** Roll the Dice Button Click Handler *************************
+        private void btnRollDice_Click(object sender, EventArgs e)
+        {
+            // call RolltheDice() method to roll the two dices for the first time
+            RolltheDice();
+            // print the number of dice1 and the number of dice2 and the total to the first Label.
+            lblTotal1.Text = dice1 + " + " + dice2 + " = " + total.ToString();
+
+            // call RolltheDice() method to roll the two dices for the second time
+            RolltheDice();
+            // print the number of dice1 and the number of dice2 and the total to the second Label.
+            lblTotal2.Text = dice1 + " + " + dice2 + " = " + total.ToString();
+
+            // add the total number of the first roll and the second roll.
+            total += total;
+
+            // if the total equals to 21,
+            if (total == 21)
+            {
+                // pop up the message to inform the user wins the game.
+                MessageBox.Show("You won!");
+                // disable the roll the dice button.
+                btnRollDice.Enabled = false;
+            }
+            // if the total equals to or more than 15
+            else if(total >= 15)
+            {
+                // pop up the message to inform that the user can roll one more dice
+                MessageBox.Show("Roll the Third Dice!");
+                // disable the first dice and the second dice
+                lblDice1.Enabled = false;
+                lblDice2.Enabled = false;
+                // enable the third dice
+                lblDice3.Enabled = true;
+
+                // call the DiceThree() method for the third dice.
+                DiceThree();
+                // set the random number between 1 to 6 to dice3.
+                dice3 = random.Next(1, 6);
+                // print the random number to the dice3 label.
+                lblDice3.Text = dice3.ToString();
+
+                // add the dice3 number to the total number
+                total += dice3;
+                // print the total number
+                lblTotal3.Text = total.ToString();
+                
+                // if the total number is equal to 21, pop up the winning message and disable the roll dice button.
+                if (total == 21)
+                {
+                    MessageBox.Show("You won!");
+                    btnRollDice.Enabled = false;
+                }
+                // else, pop up the losing message and disable the roll dice button.
+                else
+                {
+                    MessageBox.Show("You lose!");
+                    btnRollDice.Enabled = false;
+                }
+            }
+            // else, pop up the losing message and disable the roll dice button
+            else
+            {
+                MessageBox.Show("You lose!");
+                btnRollDice.Enabled = false;
+            }
+            
+        }// end the roll dice button click handler
+
+        //*********************** Reset Button Click Hander *************************
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // enable the roll the dice button
+            btnRollDice.Enabled = true;
+            
+            // enable the dice1 and dice2 
+            lblDice1.Enabled = true;
+            lblDice2.Enabled = true;
+            // disable the dice3
+            lblDice3.Enabled = false;
+
+            // set three dices to null
+            lblDice1.Text = "";
+            lblDice2.Text = "";
+            lblDice3.Text = "";
+
+            // set three total labels to null
+            lblTotal1.Text = "";
+            lblTotal2.Text = "";
+            lblTotal3.Text = "";
+
+            // set total to zero
+            total = 0;
+        }
+
+        //************************ RolltheDice() Method *******************
+        private void RolltheDice()
+        {
+            // Call the DiceOne() method
+            DiceOne();
+            // set the dice1 as the random number between 1 to 6
+            dice1 = random.Next(1, 6);
+            // print the random number
+            lblDice1.Text = dice1.ToString();
+
+            // Call the DiceTwo() method
+            DiceTwo();
+            // set the dice2 as the random number between 1 to 6
+            dice2 = random.Next(1, 6);
+            // print the random number 
+            lblDice2.Text = dice2.ToString();
+
+            // add the dice1 and dice2 number to total
+            total = dice1 + dice2;
+
+        }
+
+        //********************* DiceOne() method **********************
+        private void DiceOne()
+        {
+            switch (dice1)
+            {
+                case 1:
+                    lblDice1.Text = "1";
+                    break;
+                case 2:
+                    lblDice1.Text = "2";
+                    break;
+                case 3:
+                    lblDice1.Text = "3";
+                    break;
+                case 4:
+                    lblDice1.Text = "4";
+                    break;
+                case 5:
+                    lblDice1.Text = "5";
+                    break;
+                case 6:
+                    lblDice1.Text = "6";
+                    break;
+            }
+        }
+        //*************************** DiceTwo() method *********************
+        private void DiceTwo()
+        {
+            switch (dice2)
+            {
+                case 1:
+                    lblDice2.Text = "1";
+                    break;
+                case 2:
+                    lblDice2.Text = "2";
+                    break;
+                case 3:
+                    lblDice2.Text = "3";
+                    break;
+                case 4:
+                    lblDice2.Text = "4";
+                    break;
+                case 5:
+                    lblDice2.Text = "5";
+                    break;
+                case 6:
+                    lblDice2.Text = "6";
+                    break;
+            }
+        }
+        //************************ DiceThree() method **********************
+        private void DiceThree()
+        {
+            switch (dice3)
+            {
+                case 1:
+                    lblDice3.Text = "1";
+                    break;
+                case 2:
+                    lblDice3.Text = "2";
+                    break;
+                case 3:
+                    lblDice3.Text = "3";
+                    break;
+                case 4:
+                    lblDice3.Text = "4";
+                    break;
+                case 5:
+                    lblDice3.Text = "5";
+                    break;
+                case 6:
+                    lblDice3.Text = "6";
+                    break;
+            }
+        }
+    }
+}
